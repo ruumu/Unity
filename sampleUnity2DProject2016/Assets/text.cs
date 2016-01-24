4c3f2645bb834d4f36302064551a681a3248b641
ruumu <zugogge2@yahoo.co.jp>
2016-01-24 20:23:08 +0900

    ゲームっぽくなってきたよ！

---
+++ sampleUnity2DProject2016/Assets/text.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class text : MonoBehaviour {
+
+    public string[] scenarios; // シナリオを格納する
+    public Text uiText; // uiTextへの参照を保つ
+
+    int currentLine = 0; // 現在の行番号
+
+    void Start()
+    {
+        TextUpdate();
+    }
+
+    void Update()
+    {
+        // 現在の行番号がラストまで行ってない状態でクリックすると、テキストを更新する
+        if (currentLine < scenarios.Length && Input.GetMouseButtonDown(0))
+        {
+            TextUpdate();
+        }
+    }
+
+    // テキストを更新する
+    void TextUpdate()
+    {
+        // 現在の行のテキストをuiTextに流し込み、現在の行番号を一つ追加する
+        uiText.text = scenarios[currentLine];
+        currentLine++;
+    }
+}
